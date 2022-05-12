1ce4cfc800570ecdcf85e9f8a390e278bfae84a2
andre71-717 <manch.simonyan787.ads@gmail.com>
2022-05-12 20:57:36 +0400

    Xicon

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let myButton = document.getElementById("myButton")
 let myWeatherResult2 = document.getElementsByClassName("myWeatherResult2")[0]
-let xIcon = document.querySelector("i")
+// let xIcon = document.querySelector("i")
 
 myButton.addEventListener('click', function(){
     let createDiv = document.createElement('div');
+    let xIcon = document.createElement('i');
     createDiv.classList.add("myWeatherResult")
+    xIcon.classList.add("fa-solid" ,"fa-xmark")
+    createDiv.appendChild(xIcon)
     myWeatherResult2.appendChild(createDiv)
     createDiv.animate(
       { transform: 'translateY(0)'}, 
@@ -15,9 +18,9 @@ myButton.addEventListener('click', function(){
       createDiv.style.margin = "15px"
       createDiv.style.flexBasis = "20%"
       
-      for(let i = 0; i < createDiv.length; i++){
-        createDiv[i].appendChild(xIcon)
-      }
+      // for(let i = 0; i < createDiv.length; i++){
+       
+      // }
 
 })
 
