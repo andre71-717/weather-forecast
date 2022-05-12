e1b2f3cab0b9079ebe7418cd03fd43050a8e0109
andre71-717 <manch.simonyan787.ads@gmail.com>
2022-05-12 20:38:22 +0400

    working on icon

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ myButton.addEventListener('click', function(){
     let createDiv = document.createElement('div');
     createDiv.classList.add("myWeatherResult")
     myWeatherResult2.appendChild(createDiv)
-    createDiv.appendChild(xIcon)
     createDiv.animate(
       { transform: 'translateY(0)'}, 
       {
@@ -15,7 +14,10 @@ myButton.addEventListener('click', function(){
       })
       createDiv.style.margin = "15px"
       createDiv.style.flexBasis = "20%"
-
+      
+      for(let i = 0; i < createDiv.length; i++){
+        createDiv[i].appendChild(xIcon)
+      }
 
 })
 
