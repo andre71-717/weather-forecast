0f9f784f6d1240b58c723c6fe4e81bccb02c2587
andre71-717 <manch.simonyan787.ads@gmail.com>
2022-05-12 20:02:23 +0400

    first commit2

---
+++ index.js
@@ -0,0 +1,17 @@
+let myButton = document.getElementById("myButton")
+let myWeatherResult2 = document.getElementsByClassName("myWeatherResult2")[0]
+
+myButton.addEventListener('click', function(){
+    let createDiv = document.createElement('div');
+    createDiv.classList.add("myWeatherResult")
+    myWeatherResult2.appendChild(createDiv)
+    createDiv.animate(
+      { transform: 'translateY(0)'}, 
+      {
+        duration: 500,
+        fill: 'forwards'
+      })
+
+})
+
+
