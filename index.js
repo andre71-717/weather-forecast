2a19e719117fa5d11518b1e4ffc80a0ec0dcc628
andre71-717 <manch.simonyan787.ads@gmail.com>
2022-05-12 20:30:39 +0400

    xIcon

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let myButton = document.getElementById("myButton")
 let myWeatherResult2 = document.getElementsByClassName("myWeatherResult2")[0]
+let xIcon = document.querySelector("i")
 
 myButton.addEventListener('click', function(){
     let createDiv = document.createElement('div');
     createDiv.classList.add("myWeatherResult")
     myWeatherResult2.appendChild(createDiv)
+    createDiv.appendChild(xIcon)
     createDiv.animate(
       { transform: 'translateY(0)'}, 
       {
@@ -12,7 +14,10 @@ myButton.addEventListener('click', function(){
         fill: 'forwards'
       })
       createDiv.style.margin = "15px"
+      createDiv.style.flexBasis = "20%"
+
 
 })
+
 
 
